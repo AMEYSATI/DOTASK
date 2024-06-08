161b1d528bb6ace790f58fd90f5474a09afc7527
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2024-06-09 00:39:11 +0530

    Update server to use Neon PostgreSQL

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import pg from 'pg';
+import 'dotenv/config'
 
 const db = new pg.Client({
     user: "postgres",
